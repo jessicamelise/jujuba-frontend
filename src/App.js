518b20cd09e6f7fd26b8427a3a5c74d2f3fe531d
jessicamelise <jessica.melise@hotmail.com>
2022-09-18 20:01:52 -0300

    chore: test deploy route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Login } from './pages/login/Login';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.BASENAME_PATH}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/" element={<Navigate to="/login"/>} />
         <Route path="/login" element={<Login />} />
