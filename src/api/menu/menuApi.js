96306427d14c4810fb089a174f980f8289675c20
jessicamelise <jessica.melise@hotmail.com>
2022-09-26 22:58:17 -0300

    feat: add api changes

--- src/api/menu/menuApi.js
+++ src/api/menu/menuApi.js
@@ -17,10 +17,11 @@ export const registerNewUser = async (newUser) => {
     };
 };
 
-export const getAllUsers = async () => {
+export const login = async (email, password) => {
     try {
-        const usersAPI = await fetch("https://jujuba-api.herokuapp.com/users", {
-        // const usersAPI = await fetch("http://localhost:8000/users", {
+        const query = `?email_user=${email}&password=${password}`;
+        // const usersAPI = await fetch(`https://jujuba-api.herokuapp.com/login/${query}`, {
+        const usersAPI = await fetch(`http://localhost:8000/login${query}`, {
             method: 'GET',
         })
         const response = await usersAPI.json();
